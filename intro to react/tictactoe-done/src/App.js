ce4e2d4b9ca9d0f933e98dbba842a0654c18259e
nmcc1212 <48334675+nmcc1212@users.noreply.github.com>
2023-10-02 16:21:21 +0100

    add x and o

--- intro to react/tictactoe-done/src/App.js
+++ intro to react/tictactoe-done/src/App.js
@@ -1,18 +1,32 @@
 import { useState } from 'react';
 
 function Square({ value, onSquareClick }) {
-  return <button className="square" onClick={onSquareClick}>{value}</button>;
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
-function handleClick(i) {
-  const nextSquares = squares.slice();
-  nextSquares[i] = "X";
-  setSquares(nextSquares);
-}
-  
+  function handleClick(i) {
+    if (squares[i]) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+
   return (
     <>
       <div className="board-row">
