b4f872b0be5538f51e6e9c3c68be46ac2e1d03b1
nmcc1212 <48334675+nmcc1212@users.noreply.github.com>
2023-10-02 11:48:12 +0100

    init react

---
+++ intro to react/t3st/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import Example from './notApp.js'
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Example/>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+// create example class component
+
+export default App;
