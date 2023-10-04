2b358701ccc2b011ee2e3dea78bcc2784be9b76b
nmcc1212 <48334675+nmcc1212@users.noreply.github.com>
2023-10-04 15:31:41 +0100

    title

--- intro to react/fun-with-react/src/navBar.js
+++ intro to react/fun-with-react/src/navBar.js
@@ -5,7 +5,7 @@ const NavBar = () => {
     <div >
         <nav class="border-blue-200 bg-blue-200 dark:bg-blue-200 dark:border-blue-200">
             <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <p>My Blog</p> {/*make title */}
+                <p>The Best News</p> {/*make title */}
                 <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                     <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
