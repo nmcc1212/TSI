23530f18df25573f29b948c9aa1ada07dc3329b0
Nial <48334675+nmcc1212@users.noreply.github.com>
2023-10-05 19:57:11 +0100

    Revert "add defense news"
    This reverts commit e66fae9669003534f4cad147c80061fd0a5fd893.

--- intro to react/fun-with-react/src/navBar.js
+++ intro to react/fun-with-react/src/navBar.js
@@ -30,9 +30,6 @@ function NavBar({sendQueryToParent}) {
                                 <Link class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/HackerNews">Hacker News</Link>
                             </li>
                             <li>
-                                <Link class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" to="/DefenceNews">Defence</Link>
-                            </li>
-                            <li>
                                 <div className="flex items-center">
                                     <div className="flex border border-purple-200 rounded">
                                         <input
