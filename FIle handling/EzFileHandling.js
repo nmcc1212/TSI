9fc505f22c4052e440c4d5a094239dc8633a1a58
Nial <48334675+nmcc1212@users.noreply.github.com>
2023-10-26 10:26:24 +0100

    sync functions

--- FIle handling/EzFileHandling.js
+++ FIle handling/EzFileHandling.js
@@ -5,32 +5,65 @@ const fs = require('fs');
 const fileName = 'newFile.txt';
 const content = 'This is a new file';
 
-fs.open(fileName, 'w', (err, file) => {
-    fs.appendFile(fileName, content, (err) => {
-        if (err) throw err;
-        console.log('Saved!');
-    }
-    );
+
+try {
+    fs.writeFileSync(fileName, content);
+    console.log('Saved Text!');
 }
-);
+catch (err) {
+    console.error(err);
+}
+
+// fs.open(fileName, 'w', (err, file) => {
+//     fs.appendFile(fileName, content, (err) => {
+//         if (err) throw err;
+//         console.log('Saved Text!');
+//     }
+//     );
+// }
+// );
 
 // Json file handling
 const jsFileName = 'newJsonFile.json';
 const jsContent = '{"name":"John", "age":30, "city":"New York"}';
 
-fs.open(jsFileName, 'w', (err, file) => {
-    fs.appendFile(jsFileName, jsContent, (err) => {
-        if (err) throw err;
-        console.log('Saved!');
-    }
-    );
+try {
+    fs.writeFileSync(jsFileName, jsContent);
+    console.log('Saved JSON!');
+    const data = fs.readFileSync(jsFileName, 'utf8');
+    console.log(data);
 }
-);
+catch (err) {
+    console.error(err);
+}
 
-//
+// fs.open(jsFileName, 'w', (err, file) => {
+//     fs.appendFile(jsFileName, jsContent, (err) => {
+//         if (err) throw err;
+//         console.log('Saved JSON!');
+//     },
+//     fs.readFile(jsFileName, 'utf8', (err, data) => {
+//         console.log(data);
+//         if (err) throw err;
+//     })
+//     );
+// }
+// );
+
+// Read a json file
 const existingJsFile = 'vscode.json';
-fs.readFile(existingJsFile, (err, data) => {
-    if (err) throw err;
+
+try {
+    const data = fs.readFileSync(existingJsFile, 'utf8');
     const obj = JSON.parse(data);
     console.log(JSON.stringify(obj, null, 2));
-});
+}
+catch (err) {
+    console.error(err);
+}
+
+// fs.readFile(existingJsFile, (err, data) => {
+//     if (err) throw err;
+//     const obj = JSON.parse(data);
+//     console.log(JSON.stringify(obj, null, 2));
+// });
