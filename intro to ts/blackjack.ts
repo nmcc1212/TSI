83ec49fddd298d707edbb855704c02384d00934c
Nial <48334675+nmcc1212@users.noreply.github.com>
2023-11-09 17:31:03 +0000

    formatting

--- intro to ts/blackjack.ts
+++ intro to ts/blackjack.ts
@@ -1,116 +1,134 @@
-const aprompt = require('prompt-sync')();
+import promptSync from "prompt-sync";
+
+const prompt = promptSync();
 
 class Card {
-    constructor(public value: number, public suit: string) { }
+  constructor(public value: number, public suit: string) {}
 }
 
 class Deck {
-    cards: Card[] = [];
-    constructor() {
-        const suits = ['hearts', 'diamonds', 'spades', 'clubs'];
-        for (let i = 0; i < suits.length; i++) { // 4 suits
-            for (let j = 1; j <= 13; j++) { // 13 cards in a suit
-                this.cards.push(new Card(j, suits[i])); // push a new card with j value and suit
-            }
-        }
+  cards: Card[] = [];
+
+  constructor() {
+    const suits = ["hearts", "diamonds", "spades", "clubs"];
+    for (let i = 0; i < suits.length; i++) {
+      for (let j = 1; j <= 13; j++) {
+        this.cards.push(new Card(j, suits[i]));
+      }
     }
-    deal() {
-        return this.cards.pop();
+  }
+
+  shuffle() {
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
     }
-    shuffle() {
-        for (let i = this.cards.length - 1 ; i > 0; i--) {
-            const randomIndex = Math.floor(Math.random() * (i + 1));
-            const temp = this.cards[i];
-            this.cards[i] = this.cards[randomIndex];
-            this.cards[randomIndex] = temp;
-        }
-    }
+  }
+
+  deal() {
+    return this.cards.pop();
+  }
 }
 
 class Player {
-    hand: Card[] = [];
-    constructor(public name: string) { }
+  hand: Card[] = [];
 
-    get score() {
-        let score = 0
-        let hasAce = false
-        for (let i=0; i < this.hand.length; i++) {
-            const card = this.hand[i]
-            if (card.value === 1 ) {
-                hasAce = true
-            }
-            score += Math.min(card.value, 10) // card value or 10 for face cards
+  constructor(public name: string) {}
 
-        }
-        if (hasAce && score + 10 <= 21) { // if ace, and score + 10 is less than 21, add 10
-            score += 10
-        }
-        return score
+  get score() {
+    let score = 0;
+    let hasAce = false;
+    for (let i = 0; i < this.hand.length; i++) {
+      const card = this.hand[i];
+      if (card.value === 1) {
+        hasAce = true;
+      }
+      score += Math.min(card.value, 10);
     }
-    get isBusted() {
-        return this.score > 21
+    if (hasAce && score + 10 <= 21) {
+      score += 10;
     }
-    get isBlackjack() {
-        return this.score === 21 && this.hand.length === 2
-    }
-    hit (card: Card) {
-        if (card) {
-            this.hand.push(card);
-        }
-    }
+    return score;
+  }
 
+  get isBusted() {
+    return this.score > 21;
+  }
 
+  get isBlackjack() {
+    return this.score === 21 && this.hand.length === 2;
+  }
+
+  hit(card: Card) {
+    if (card) {
+      this.hand.push(card);
+    }
+  }
+
+  asciiVersionOfHand() {
+    const lines = ["", "", "", "", "", "", "", "", ""];
+    for (let i = 0; i < this.hand.length; i++) {
+      const card = this.hand[i];
+      const rank = card.value === 1 ? "A" : card.value === 11 ? "J" : card.value === 12 ? "Q" : card.value === 13 ? "K" : card.value.toString();
+      const suit = card.suit === "hearts" ? "♥" : card.suit === "diamonds" ? "♦" : card.suit === "spades" ? "♠" : "♣";
+      lines[0] += "┌─────────┐ ";
+      lines[1] += `│${rank.padEnd(2)}       │ `;
+      lines[2] += "│         │ ";
+      lines[3] += `│    ${suit}    │ `;
+      lines[4] += "│         │ ";
+      lines[5] += `│       ${rank.padStart(2)}│ `;
+      lines[6] += "└─────────┘ ";
+    }
+    return lines.join("\n");
+  }
 }
 
-class Main {
-    deck: Deck
-    player: Player
-    dealer: Player
+class Blackjack {
+  deck: Deck;
+  dealer: Player;
+  player: Player;
 
-    constructor() {
-        this.deck = new Deck()
-        this.deck.shuffle()
-        this.player = new Player('Player')
-        this.dealer = new Player('Dealer')
-        this.player.hit(this.deck.deal()!)
-        this.dealer.hit(this.deck.deal()!)
-        this.player.hit(this.deck.deal()!)
-        this.dealer.hit(this.deck.deal()!)
-    }
+  constructor() {
+    this.deck = new Deck();
+    this.deck.shuffle();
+    this.dealer = new Player("Dealer");
+    this.player = new Player("Player");
+    this.player.hit(this.deck.deal()!);
+    this.dealer.hit(this.deck.deal()!);
+    this.player.hit(this.deck.deal()!);
+    this.dealer.hit(this.deck.deal()!);
+  }
 
-    get isGameOver() {
-        return this.player.isBusted || this.dealer.isBusted || this.player.hand.length === 5 || this.player.isBlackjack || this.dealer.isBlackjack; // if player is busted, dealer is busted, player has 5 cards, player has blackjack, dealer has blackjack
-      }
-    
-    get winner() {
-        if (this.player.isBusted || this.dealer.isBlackjack || (!this.player.isBlackjack && this.dealer.isBlackjack) || (this.dealer.score > this.player.score && !this.dealer.isBusted)) { // if player is busted, dealer has blackjack, player doesn't have blackjack, dealer has higher score and isn't busted
-            return this.dealer;
-        } else if (this.dealer.isBusted || this.player.isBlackjack || (!this.dealer.isBlackjack && this.player.isBlackjack) || (this.player.score > this.dealer.score && !this.player.isBusted)) {
-            return this.player;
-        } else {
-            return null;
-        }
+  get isGameOver() {
+    return this.player.isBusted || this.dealer.isBusted || this.player.hand.length === 5 || this.player.isBlackjack || this.dealer.isBlackjack;
+  }
+
+  get winner() {
+    if (this.player.isBusted || this.dealer.isBlackjack || (!this.player.isBlackjack && this.dealer.isBlackjack) || (this.dealer.score > this.player.score && !this.dealer.isBusted)) {
+      return this.dealer;
+    } else if (this.dealer.isBusted || this.player.isBlackjack || (!this.dealer.isBlackjack && this.player.isBlackjack) || (this.player.score > this.dealer.score && !this.player.isBusted)) {
+      return this.player;
+    } else {
+      return null;
     }
+  }
+
     play() {
         while (!this.isGameOver) {
-            if (this.player.score < 21) {
-                let move:String = aprompt(`Your hand: ${this.player.hand.map(card => card.value + card.suit).join(", ")}. Hit?`);
-                if (move === "y") {
-                    this.player.hit(this.deck.deal()!);
-                }
+            if (this.player.score < 21 && prompt(`Your hand:  ${this.player.asciiVersionOfHand()}!?`) === "y") {
+                this.player.hit(this.deck.deal()!);
             }
-              if (this.dealer.score < 17) {
+            if (this.dealer.score < 17) {
                 this.dealer.hit(this.deck.deal()!);
-              }
-            }
-            const winner = this.winner;
-            if (winner) {
-                console.log(`${winner.name} wins, with a score of ${winner.score}`);
-            } else {
-                console.log("Draw.");
             }
         }
+        const winner = this.winner;
+        if (winner) {
+            console.log(`The winner is ${winner.name} with a score of ${winner.score}!`);
+        }
+        else {
+            console.log(`It's a tie!`);
+        }
     }
-
-const game = new Main()
-game.play()
+}
+const game = new Blackjack();
