f91594db67125d0028113ac6429baa85d9a9f2ca
Nial <48334675+nmcc1212@users.noreply.github.com>
2023-12-15 09:43:05 +0000

    gh action fix

--- intro to react/vite-project/test/test_newsList.test.js
+++ intro to react/vite-project/test/test_newsList.test.js
@@ -5,7 +5,7 @@ import '@testing-library/jest-dom'
 import React from 'react';
 import { render, screen, act } from '@testing-library/react';
 import axios from 'axios';
-import NewsList from '../src/NewsList';
+import NewsList from '../src/NewsList.tsx';
 
 // Mocking axios
 jest.mock('axios');
