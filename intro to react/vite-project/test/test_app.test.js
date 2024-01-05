4d707dce4b920afc8de8b51ed39db78057b807fb
Nial <48334675+nmcc1212@users.noreply.github.com>
2024-01-05 17:26:52 +0000

    end of day nothing changed

--- intro to react/vite-project/test/test_app.test.js
+++ intro to react/vite-project/test/test_app.test.js
@@ -4,7 +4,6 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import userEvent from '@testing-library/user-event';
-import { act } from 'react-dom/test-utils';
 import '@testing-library/jest-dom';
 import App from '../src/App';
 
