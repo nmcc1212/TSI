c05861e356b1faeec1f89d12a56f09b1854496d5
Nial <48334675+nmcc1212@users.noreply.github.com>
2024-02-15 22:27:15 +0000

    get route for users

--- newAPI/src/routes/userRoutes.ts
+++ newAPI/src/routes/userRoutes.ts
@@ -3,18 +3,40 @@ import User from "../schemas/usersSchema";
 
 const userRouter = Router();
 
+// username, email, and password required
 userRouter.post("/", async (req: Request, res: Response) => {
-    if (!req.body.name || !req.body.email || !req.body.password) {
-        return res.status(400).json({ message: 'Name, email and password are required' });
+    if (!req.body.username || !req.body.email || !req.body.password) {
+        return res.status(400).json({ message: 'username, email and password are required' });
     }
     const user = new User({
-        name: req.body.name,
+        username: req.body.username,
         email: req.body.email,
         password: req.body.password,
     });
     console.log("Created new user: ", user, "with id: ", user._id, "and email: ", user.email), "and password: ", user.password;
     await user.save();
     res.json(user);
+});
+
+
+// can take _id, email, or username as query parameter
+userRouter.get("/", async (req: Request, res: Response) => {
+    if (req.query._id !== undefined) {
+        let users = await User.find({ _id: req.query._id });
+        return res.json(users);
+    }
+    else if (req.query.email !== undefined) {
+        let users = await User.find({ email: req.query.email });
+        return res.json(users);
+    }
+    else if (req.query.username !== undefined) {
+        let users = await User.find({ username: req.query.username });
+        return res.json(users);
+    }
+    else {
+        let users = await User.find();
+        return res.json(users);
+    }
 });
 
 export default userRouter;
