4ce9c3a19320903cdbfc4293c6bcbcd17a9e940c
Nial <48334675+nmcc1212@users.noreply.github.com>
2024-02-15 23:41:50 +0000

    Add bcrypt for password hashing and validate unique username and email

--- newAPI/src/routes/userRoutes.ts
+++ newAPI/src/routes/userRoutes.ts
@@ -1,5 +1,6 @@
 import { Router, Response, Request } from "express";
 import User from "../schemas/usersSchema";
+import bcrypt from "bcrypt";
 
 const userRouter = Router();
 
@@ -8,10 +9,24 @@ userRouter.post("/", async (req: Request, res: Response) => {
     if (!req.body.username || !req.body.email || !req.body.password) {
         return res.status(400).json({ message: 'username, email and password are required' });
     }
+    // check if user already exists
+    const email = req.body.email;
+    const username = req.body.username;
+    const plainPassword = req.body.password;
+
+    const usernameExists = await User.findOne({username});
+    if (usernameExists) {
+        return res.status(400).json({ message: 'username already exists' });
+    }
+    // check if email already exists
+    const emailExists = await User.findOne({email});
+    if (emailExists) {
+        return res.status(400).json({ message: 'email already exists' });
+    }
     const user = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password,
+        username: username,
+        email: email,
+        password: plainPassword,
     });
     console.log("Created new user: ", user, "with id: ", user._id, "and email: ", user.email), "and password: ", user.password;
     await user.save();
@@ -38,5 +53,7 @@ userRouter.get("/", async (req: Request, res: Response) => {
         return res.json(users);
     }
 });
+
+
 
 export default userRouter;
