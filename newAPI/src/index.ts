a380557239f596758494847289eb93625fe3e92a
Nial <48334675+nmcc1212@users.noreply.github.com>
2024-02-16 15:35:36 +0000

    Add logger middleware to app

--- newAPI/src/index.ts
+++ newAPI/src/index.ts
@@ -5,6 +5,7 @@ import Post from "./schemas/postsSchema";
 import User from "./schemas/usersSchema";
 import postRouter from "./routes/postRoutes";
 import userRouter from "./routes/userRoutes";
+import loggerMiddleware from "./middlewares/logger";
 
 
 
@@ -12,6 +13,7 @@ const app = express();
 const port = 3101;
 
 app.use(express.json());
+app.use(loggerMiddleware);
 
 app.use("/posts",postRouter);
 app.use("/users",userRouter);
