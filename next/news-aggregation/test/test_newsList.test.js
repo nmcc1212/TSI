d00006814052230c0016f780722fdaa65f0800e6
Nial <48334675+nmcc1212@users.noreply.github.com>
2024-04-05 10:43:36 +0100

    Fix import casing in test_newsList.test.js

--- next/news-aggregation/test/test_newsList.test.js
+++ next/news-aggregation/test/test_newsList.test.js
@@ -5,7 +5,7 @@ import '@testing-library/jest-dom'
 import React from 'react';
 import { render, screen, act } from '@testing-library/react';
 import axios from 'axios';
-import NewsList from '../src/components/NewsList'
+import NewsList from '../src/components/newsList'
 
 // Mocking axios
 jest.mock('axios');
