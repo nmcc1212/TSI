4e4a4369a0fb4c7f6f9964d4892bb460d06bff23
Nial <48334675+nmcc1212@users.noreply.github.com>
2024-04-18 10:05:46 +0100

    Add playwright tests

--- next/news-aggregation/tests/nav.spec.ts
+++ next/news-aggregation/tests/nav.spec.ts
@@ -1,6 +1,27 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('nav to ukraine', async ({ page }) => {
     await page.goto('http://localhost:3000/');
-    
-    
+    await page.locator('#navbar-default :text("Ukraine News")').click();
+    await expect(page).toHaveURL(/.*\/Ukraine/);
+});
+test('nav to UK', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    await page.locator('#navbar-default :text("UK News")').click();
+    await expect(page).toHaveURL(/.*\/UKNews/);
+});
+test('nav to World News', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    await page.locator('#navbar-default :text("World News")').click();
+    await expect(page).toHaveURL(/.*\/WorldNews/);
+});
+test('nav to World News', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    await page.locator('#navbar-default :text("Hacker News")').click();
+    await expect(page).toHaveURL(/.*\/HackerNews/);
+});
+test('nav to World News', async ({ page }) => {
+    await page.goto('http://localhost:3000/');
+    await page.locator('#navbar-default :text("Defence News")').click();
+    await expect(page).toHaveURL(/.*\/DefenceNews/);
+});
